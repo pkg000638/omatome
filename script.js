9b08880dab929ef0980e24390ed9935b4a87a572
pkg000638 <127655631+pkg000638@users.noreply.github.com>
2023-03-12 17:52:46 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function calculate() {
+  const car = document.getElementById("car").value;
+  let result = "";
+  switch (car) {
+    case "A":
+      result = "48,552";
+      break;
+    case "B":
+      result = "39,978";
+      break;
+    case "C":
+      result = "50,260";
+      break;
+    case "D":
+      result = "41,445";
+      break;
+    case "E":
+      result = "69,778";
+      break;
+    case "F":
+      result = "57,051";
+      break;
+    case "G":
+      result = "71,663";
+      break;
+    case "H":
+      result = "58,947";
+      break;
+    case "I":
+      result = "79,048";
+      break;
+    case "J":
+      result = "66,174";
+      break;
+    case "K":
+      result = "52,902";
+      break;
+    default:
+      result = "エラーが発生しました。";
+  }
+  document.getElementById("result").textContent = `月額: ${result}円`;
+}
